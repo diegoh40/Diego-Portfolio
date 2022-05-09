06d37a35f8c8e09c4d1f4df1d9d525864c766854
Gonzalo <mgmediaweb@gmail.com>
2022-05-09 14:44:45 -0700

    mobile menu with issues

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function menu_close() {
+    let mmenu = document.querySelector('#mobile_menu');
+    mmenu.style.display = 'none';    
+}
+
+function menu_open() {
+    let mmenu = document.querySelector('#mobile_menu');
+    mmenu.style.display = 'block';    
+}
